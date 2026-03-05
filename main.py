3570a411b9f97497fba278e93400fa5228bf42d1
nodedevist <129120614+nodedevist@users.noreply.github.com>
2026-03-05 23:52:12 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:51:53.294Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T20:52:11.589Z
+Commit: Resolve null pointer exception in user service
