066e50a5c00a3f4be4ebd552f0ec2bb50b243236
nodedevist <129120614+nodedevist@users.noreply.github.com>
2026-03-05 23:51:50 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:16:14.811Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:51:49.869Z
+Commit: Patch API endpoint error handling
